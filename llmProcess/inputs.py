c1659436ed2657f192efc5dc65389d3a8244c82e
ben_mcwilliams <bmmcw4@gmail.com>
2025-04-21 18:54:37 +0200

    before updating finetuning code

--- llmProcess/inputs.py
+++ llmProcess/inputs.py
@@ -11,7 +11,9 @@ groups_to_prompts = {
     "ownership": "prompts/ownership.txt",
     "technological": "prompts/technological.txt",
     "financial_origin": "prompts/financial-origin.txt",
-    "financial_technological": "prompts/financial-technological.txt"
+    "financial_technological": "prompts/financial-technological.txt",
+    "capacities": "prompts/capacities.txt",
+    "investments": "prompts/investments.txt"
 }
 
 # which node types are allowed for the query to return a group of relationships
@@ -19,5 +21,19 @@ nodes_by_group_prompt = {
     "ownership": ["company", "joint_venture", "factory"],
     "technological": ["factory", "capacity", "product"],
     "financial_origin": ["company", "joint_venture"],
-    "financial_technological": ["investment", "capacity", "factory", "product"]
+    "financial_technological": ["investment", "capacity", "factory", "product"],
+    "capacities": ["capacity"],
+    "investments": ["investment"]
+}
+
+# keys for extracting characteristics about particular nodes
+characteristic_node_types = {
+    "capacities": {
+        "id_key": "capacity_ID",
+        "type_match": "capacity"
+    },
+    "investments": {
+        "id_key": "investment_ID",
+        "type_match": "investment"
+    }
 }
--- llmProcess/model_dictionary.py
+++ llmProcess/model_dictionary.py
@@ -1,7 +1,9 @@
 model_dictionary = {
-    "nodes": "ft:gpt-4o-mini-2024-07-18:personal:finetuning-nodes:BIDvMUVl",
-    "ownership": "ft:gpt-4o-mini-2024-07-18:personal:ownership-relations:BIF9Uu4b",
-    "technological": "gpt-4o-mini",
-    "financial_origin": "gpt-4o-mini",
-    "financial_technological": "gpt-4o-mini"
+    "nodes": "gpt-4o",
+    "ownership": "gpt-4o",
+    "technological": "gpt-4o",
+    "financial_origin": "gpt-4o",
+    "financial_technological": "gpt-4o",
+    "capacities": "gpt-4o",
+    "investments": "gpt-4o"
 }   
