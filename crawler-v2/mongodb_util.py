bf4b5a952b2e38289373a3329f76b2c908c5e618
ben_mcwilliams <bmmcw4@gmail.com>
2025-05-15 14:59:52 +0200

    cleaning explore_database.ipynb

--- crawler-v2/mongodb_util.py
+++ crawler-v2/mongodb_util.py
@@ -7,14 +7,12 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("MONGO_DB_NAME")
 
-
 def get_mongo_collection(collection_name=None):
     """Returns the MongoDB collection. Defaults to MONGO_URLS_NAME if not provided."""
     client = MongoClient(MONGO_URI)
     db = client[DB_NAME]
     collection_name = collection_name or os.getenv("MONGO_URLS_NAME")
     return db[collection_name]
-
 
 def get_existing_urls(collection, category):
     """Fetch existing URLs from MongoDB collection for a specific category."""
