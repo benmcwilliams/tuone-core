51a4e15317da25754274bbdc7621790da50ba1ed
Benjamin McWilliams <bmmcw4@gmail.com>
2025-07-01 12:10:22 +0200

    clean push

--- reconcile/src/normalise_capacities.py
+++ reconcile/src/normalise_capacities.py
@@ -1,3 +1,5 @@
 # script should take in a capacity string value and output the normalised value 
 # will involve a mix of string cleaning & some logic will be based upon the product
 # eg 100,000 vehicles which QUANTIFIES battery production needs to be normalised into battery MWh units
+
+# a function which can be applied to a pandas dataframe column of capacity strings
