ab6132d04bfb26dc85a0d57f3da4908e0d963b1c
Benjamin McWilliams <bmmcw4@gmail.com>
2025-08-04 19:20:40 +0200

    working to reconcile

--- mongo_client.py
+++ mongo_client.py
@@ -12,6 +12,7 @@ DB_NAME = os.getenv("MONGO_DB_NAME")
 ARTICLES_COLLECTION_NAME = os.getenv("MONGO_COLLECTION_NAME")
 TEST_ARTICLES_COLLECTION = os.getenv("MONGO_TEST_ARTICLES_NAME")
 CAPACITIES_COLLECTION = os.getenv("MONGO_CAPICITIES_COLLECTION")
+FACILITIES_COLLECTION = os.getenv("MONGO_FACILITIES_COLLECTION")
 
 if not all([MONGO_URI, DB_NAME, ARTICLES_COLLECTION_NAME]):
     raise RuntimeError("❌ Missing required MongoDB environment variables.")
@@ -21,5 +22,6 @@ db = mongo_client[DB_NAME]
 articles_collection = db[ARTICLES_COLLECTION_NAME]
 test_articles_collection = db[TEST_ARTICLES_COLLECTION]
 capacities_collection = db[CAPACITIES_COLLECTION]
+facilities_collection = db[FACILITIES_COLLECTION]
 # MongoDB target collection for Geonames
 geonames_collection = db["geonames_lookup"]
