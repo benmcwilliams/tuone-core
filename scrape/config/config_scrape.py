bf369889486fcdd0eb7d78f071d3ca70fa697ce9
Benjamin McWilliams <bmmcw4@gmail.com>
2025-08-05 18:37:15 +0200

    merging Joe logic

--- scrape/config/config_scrape.py
+++ scrape/config/config_scrape.py
@@ -1,5 +1,21 @@
+# HEADERS = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+# }
+
+# Headers for requests
 HEADERS = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'User-Agent': (
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+        '(KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
+    ),
+    'Accept': (
+        'text/html,application/xhtml+xml,application/xml;q=0.9,'
+        'image/avif,image/webp,image/apng,*/*;q=0.8'
+    ),
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://www.google.com/',
+    'DNT': '1',
+    'Upgrade-Insecure-Requests': '1',
 }
 
 COOKIES = {
