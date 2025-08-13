da46f4227f23dd863a1515cf2c9870e0168cc1d0
Benjamin McWilliams <bmmcw4@gmail.com>
2025-08-13 16:32:20 +0200

    run

--- llm_process_status.py
+++ llm_process_status.py
@@ -5,12 +5,13 @@ from datetime import datetime, timezone
 test_mongo_connection()
 
 # filters
-categories = ["electrive", "pvmagazine"]
-cutoff_date = datetime(2023, 1, 1)
+categories = ["user"]
+cutoff_date = datetime(2021, 1, 1)
 
 articles_to_process = list(
     articles_collection.find(
-        {"meta.date": {"$gt": cutoff_date},
+        {
+        "meta.date": {"$gt": cutoff_date},
          "meta.category": {"$in": categories}},  
         {"_id": 1, "meta": 1, "title": 1, "validation": 1, "llm_processed": 1,  "paragraphs": 1}       
     )
