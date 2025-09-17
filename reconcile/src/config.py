14b865862d41d0b479a71abb949b698d287d5ffa
Benjamin McWilliams <bmmcw4@gmail.com>
2025-09-17 09:17:04 +0200

    included investment normalisation as py

--- reconcile/src/config.py
+++ reconcile/src/config.py
@@ -14,17 +14,24 @@ ARTICLE_PROJECTION = {
     "relationships": 1
 }
 
+# builder files
 ALL_NODES = "storage/output/all_nodes.xlsx"
 ALL_RELS = "storage/output/all_rels.xlsx"
+GEO_LOOKUP_JSON = "storage/output/geo_lookup.json"
+
+# raw views
 FACTORY_REGISTRY = "storage/output/factory_registry.xlsx"
 FACTORY_TECH = "storage/output/factory-technological.xlsx"
 COMPANY_JV = "storage/output/company-joint-venture.xlsx"
 INVESTMENT_FUNDS = "storage/output/investment-funds-factory.xlsx"
 
+# cleaning capacities and investments
 FACTORY_TECH_CLEAN_CAPACITIES = "storage/output/factory-technological-clean-capacities.xlsx"
+FACTORY_TECH_CLEAN_CAPACITIES_INVESTMENTS = "storage/output/factory-technological-clean-capacities-investments.xlsx"
+CLEAN_INVESTMENT_FUNDS = "storage/output/clean-investment-funds-factory.xlsx"
 CAPACITIES_DEBUG = "storage/output/capacities-debug.xlsx"
-GEO_LOOKUP_JSON = "storage/output/geo_lookup.json"
 
+# grouping
 GROUPED_CAPACITIES = "storage/output/grouped-capacities.xlsx"
 GROUPED_FACTORIES = "storage/output/grouped-factories.xlsx"
 GROUPED_INVESTMENTS = "storage/output/grouped-investments.xlsx"
@@ -35,7 +42,6 @@ GRPD_PROJECTS_FILTER = "storage/output/filtered_projects.xlsx"
 PRODUCT_CLASSIFICATION = "storage/input/product_classification.xlsx"
 FACILITIES = "storage/output/facilities.xlsx"
 CAPACITIES_PLOT = "storage/output/capacities_plot.xlsx"
-
 
 grouped_facilities_cols = [
     "inst_canon", "inst_type", "owner_label", "iso2", "factory", "admin_group_key", 
@@ -50,7 +56,7 @@ grouped_capacities_cols = [
     "product", "product_lv1", "product_lv2", "factory_status", "date",
     "capacity", "capacity_normalized", "capacity_metric_normalized", "phase", "status", 
     "project_id", "project_key_str", "cluster_id",
-    "investment", "investment_status", "investment_phase", 
+    "investment", "amount_EUR", "investment_status", "investment_phase", 
     "lat", "lon", "city_key", "adm1", "adm1-og", "adm2", "adm3", "adm4",
     "article_id", "investment_id"
 ]
@@ -58,7 +64,7 @@ grouped_capacities_cols = [
 grouped_investments_cols = [
     "inst_canon", "inst_type", "owner_label", "iso2", "factory", "admin_group_key", 
     "product", "product_lv1", "product_lv2", "factory_status", "date",
-    "investment", "phase", "status", 
+    "investment", "amount_EUR", "phase", "status", 
     "project_id", "project_key_str", "cluster_id", 
     "lat", "lon", "city_key", "adm1", "adm1-og", "adm2", "adm3", "adm4",
     "article_id", "investment_id"
@@ -66,7 +72,7 @@ grouped_investments_cols = [
 
 GROUP_SPEC = [
         # (input_excel, output_excel, output_cols)
-        (FACTORY_TECH_CLEAN_CAPACITIES, GROUPED_CAPACITIES, grouped_capacities_cols),
+        (FACTORY_TECH_CLEAN_CAPACITIES_INVESTMENTS, GROUPED_CAPACITIES, grouped_capacities_cols),
         (FACTORY_REGISTRY,  GROUPED_FACTORIES, grouped_facilities_cols),
-        (INVESTMENT_FUNDS, GROUPED_INVESTMENTS, grouped_investments_cols)
+        (CLEAN_INVESTMENT_FUNDS, GROUPED_INVESTMENTS, grouped_investments_cols)
     ]
