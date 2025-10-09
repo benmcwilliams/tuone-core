f8996b47df5a6eae16f652c52c18e9ef8f2b2321
Benjamin McWilliams <bmmcw4@gmail.com>
2025-10-09 14:02:57 +0200

    set wind for kg_builder

--- kg_builder/main.py
+++ kg_builder/main.py
@@ -264,9 +264,11 @@ def process_articles(articles_to_process, model_dictionary):
             print(f"🔒 Closed logger for article {articleID}. Remaining handlers: {len(logger.handlers)}")
 
 offset_articles = 0
-#categories = ["user", "user_text", "electrive", "justauto", "pvmagazine", "pvtech"]
-categories = ["user", "user_text"]
-cutoff_date = datetime(2010, 1, 1)
+categories = ["user", "user_text", 
+                "electrive", "justauto", "pvmagazine", "pvtech"]
+categories = ["renewsBiz", "offshorewind"]
+#categories = ["user", "user_text"]
+cutoff_date = datetime(2021, 1, 1)
 
 articles_to_process = list(
     articles_collection.find(
