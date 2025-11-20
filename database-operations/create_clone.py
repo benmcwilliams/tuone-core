580327a9175aa2f88def066cf6f7fbd9d0d6fc5d
Benjamin McWilliams <bmmcw4@gmail.com>
2025-11-20 14:28:12 +0100

    latest

--- database-operations/create_clone.py
+++ database-operations/create_clone.py
@@ -5,9 +5,9 @@ from mongo_client import mongo_client
 
 # ---------- Configuration ----------
 SOURCE_DB_NAME = "tuone"
-SOURCE_COLLECTION_NAME = "articles"
+SOURCE_COLLECTION_NAME = "facilities_develop"
 TARGET_DB_NAME = "tuone"
-TARGET_COLLECTION_NAME = "articles_clone"
+TARGET_COLLECTION_NAME = "facilities"
 BATCH_SIZE = 1000
 
 # ---------- Setup ----------
